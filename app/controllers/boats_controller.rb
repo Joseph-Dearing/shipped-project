9d613818e938974d295539c69a82048140c9a04b
Joseph Dearing <joe@Josephs-MacBook-Pro.local>
2018-09-25 15:46:47 -0400

    CRUD functions added for boats & jobs done

---
+++ app/controllers/boats_controller.rb
@@ -0,0 +1,56 @@
+class BoatsController < ApplicationController
+    # GET /boats
+    def index
+      @boats = Boat.all
+    end
+  
+    # GET /boats/1
+    def show
+      @boat = Boat.find(params[:id])
+    end
+  
+    # GET /boats/new
+    def new
+      @boat = Boat.new
+    end
+  
+    # GET /boats/1/edit
+    def edit
+      @boat = Boat.find(params[:id])
+    end
+  
+    # POST /boats
+    def create
+      @boat = Boat.new(boat_params)
+      @boat.user_id = current_user.id
+      if @boat.save
+        redirect_to "/"
+      else
+        render "/boats/new"
+      end
+    end
+  
+    # PATCH/PUT /boats/1
+    def update
+      @boat = Boat.find(params[:id])
+      @boat.update(boat_params)
+      if @boat.save
+        redirect_to "/"
+      else
+        render "/boats/new"
+      end
+    end
+  
+    # DELETE /boats/1
+    def destroy
+      boat = Boat.find(params[:id])
+      boat.destroy
+      redirect_to boats_path
+    end
+  
+    private
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def boat_params
+        params.require(:boat).permit(:name, :max_containers, :location, :user_id)
+      end
+  end
